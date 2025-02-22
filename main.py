b8cc841b015fa022638eea7354afb5a6c292074d
Ronim10 <Ronim2122@gmail.com>
2025-02-22 13:17:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from skimage.filters import median
 from skimage.morphology import ball
+from PIL import Image
 
+image=load_image('פרח.jpg')
+plt.imshow(image)
 clean_image = median(image, ball(3))
-from PIL import Image
+edgeMAG = edge_detection(clean_image)
+plt.imshow(edgeMAG, cmap='gray')
+plt.show()
+# prompt: Convert the resulting edgeMAG array into a binary array (values of 0 and 1, or True and False) by choosing a threshold value.
+# To choose the threshold value, it is recommended to look at the histogram of the image, as demonstrated in the practice session.
 
-edge_image = Image.fromarray(edge_binary)
-edge_image.save('my_edges.png')
-from image_utils import load_image, edge_detection
-from PIL import Image
-from skimage.filters import median
-from skimage.morphology import ball
-import numpy as np
 import matplotlib.pyplot as plt
-
-perach = load_image('פרח.jpeg')
-clean_image = median(perach, ball(3))
-edge_mag = edge_detection(clean_image)
-
-edge_binary = edge_mag > 40
+threshold = 40 
+edge_binary = edgeMAG > threshold
 plt.imshow(edge_binary, cmap='gray')
-
+plt.show()
+from PIL import Image
 edge_image = Image.fromarray(edge_binary)
 edge_image.save('my_edges.png')
